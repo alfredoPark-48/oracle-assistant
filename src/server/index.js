5bde5a7f1456276a4a2222101c5fc302f6c27667
alfredoPark-48 <parkalfredojeonghyun@gmail.com>
2023-10-24 18:03:10 -0700

    Test: Added unit tests and job for automated testing

--- src/server/index.js
+++ src/server/index.js
@@ -1,16 +1,13 @@
 const express = require('express');
 const app = express();
-const PORT = process.env.PORT || 3000;
 
 const predictionRoutes = require('./routes/predictionRoutes');
 
 app.use(express.json());
 app.use(predictionRoutes);
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
-
 app.get('/', (req, res) => {
     res.send('Hello World');
 });
+
+module.exports = app;
