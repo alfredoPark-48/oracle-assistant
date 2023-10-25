7fb1965335c10378ec9f0c328e66be10275ce5fb
alfredoPark-48 <parkalfredojeonghyun@gmail.com>
2023-10-24 18:15:30 -0700

    Test: Added unit tests and job for automated testing

--- jest.config.js
+++ jest.config.js
@@ -1,14 +1,2 @@
 module.exports = {
-    rootDir: "./",
-    moduleFileExtensions: ["js", "jsx", "json", "ts", "tsx"],
-    testMatch: ["<rootDir>/__tests__/**/*.spec.ts"],
-    setupFiles: [],
-    transform: {
-        "^.+\\.(ts|tsx)$": "ts-jest",
-    },
-    testEnvironment: "node",
-    collectCoverage: true,
-    coverageDirectory: "./coverage",
-    modulePaths: ["<rootDir>/src"],
-    coveragePathIgnorePatterns: ["/node_modules/"],
 };
