a4b8a4689416f4a8a856409c0109d200d04ae0e7
alfredoPark-48 <parkalfredojeonghyun@gmail.com>
2023-11-16 18:13:11 -0600

    Feat & Refactor: Added product routes, removed unused ODA packages and modified script

---
+++ src/routes/productRoutes.js
@@ -0,0 +1,65 @@
+const mongoose = require("mongoose");
+
+const Product = mongoose.model("Product");
+
+module.exports = (app) => {
+    app.get("/product/", async (req, res) => {
+        const products = await Product.find();
+        res.send(products);
+    });
+
+    app.post("/product/", async (req, res) => {
+        const { productName, price, quantity, brandName } = req.body;
+
+        const product = new Product({
+            productName,
+            price,
+            quantity,
+            brandName,
+        });
+
+        try {
+            await product.save();
+            res.send(product);
+        } catch (error) {
+            res.status(400).send(error);
+        }
+    });
+
+    app.patch("/product/:productId", async (req, res) => {
+        const { productId } = req.params;
+        const updateFields = req.body;
+
+        try {
+            const updatedProduct = await Product.findByIdAndUpdate(
+                productId,
+                updateFields,
+                { new: true }
+            );
+
+            if (!updatedProduct) {
+                return res.status(404).send("Product not found");
+            }
+
+            res.send(updatedProduct);
+        } catch (error) {
+            res.status(400).send(error);
+        }
+    });
+
+    app.delete("/product/:productId", async (req, res) => {
+        const { productId } = req.params;
+
+        try {
+            const deletedProduct = await Product.findByIdAndDelete(productId);
+
+            if (!deletedProduct) {
+                return res.status(404).send("Product not found");
+            }
+
+            res.send(deletedProduct);
+        } catch (error) {
+            res.status(500).send(error);
+        }
+    });
+};
