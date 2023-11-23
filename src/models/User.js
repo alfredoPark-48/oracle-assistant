d63e9e7f773c679ffe8843797e136ea29ecbb2a4
feraranas <46a12sFAASfaas>
2023-11-22 18:06:47 -0600

    new post endpoint

--- src/models/User.js
+++ src/models/User.js
@@ -2,7 +2,8 @@ const mongoose = require("mongoose");
 const { Schema } = mongoose;
 
 const userSchema = new Schema({
-    displayName: String
+    displayName: String,
+    creditResult: Boolean
 })
 
 mongoose.model('User', userSchema);
